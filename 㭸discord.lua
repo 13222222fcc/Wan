cdb4e7e0a658cbfc9579844810ff6fe0ed52eece
di🥺di🤓di <xjv_2025@qq.com>
2025-12-09 13:36:15 +0800

    Add HTML structure with styled text and imagedy

--- 㭸discord.lua
+++ 㭸discord.lua
@@ -1,56 +1,29 @@
-    local library = loadstring(game:HttpGet(('https://github.com/DevSloPo/Auto/raw/main/Ware-obfuscated.lua')))()
+local library = loadstring(game:HttpGet(""))(); --这里是放你UI的地方也就是你的界面
 
-    local window = library:new("脚本名字")
-    local XKHub = window:Tab("关于", "7733774602")
-    local XK = XKHub:section("示例", true)
-    
-    XK:Label("文本标签")
-    
-    XK:Button(
-        "点击",
-        function()
-            
-        end)
-    
-    XK:Toggle("开关", "", false, function(state)
+local window = library:new("脚本") --这里是你的脚本名
 
-    end)
-    
-    XK:Slider("滑块", "", 16, 0,100, false, function(v)
+local Wans = window:Tab("通用",'图片ID') --这是选项卡必须加不然脚本用不了
 
-    end)
-    
-    XK:Dropdown("Dropdown", "", { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, function(s)
-	print(s)
-    end)
+local Wan =Wans:section("通用",true) --这个也是
 
-    XK:Textbox("输入文本", "", "输入", function(s)
-	
-    end)    
-    
-    local XKHub = window:Tab("其他", "78892482588180")
-    local XKNB = XKHub:section("内容", true)
-    
-    XKNB:Label("文本标签")
-    
-    XKNB:Button(
-        "点击",
-        function()
-            
-        end)
-    
-    XKNB:Toggle("开关", "", false, function(state)
+Wan:Button("按钮", function()
+    --当然了这里是放脚本的地方比如放皮脚本之类的东西
+end)
 
-    end)
-    
-    XKNB:Slider("滑块", "", 16, 0, 100, false, function(v)
+Wan:Toggle("开关", "Wan", false, function(a)
+    --这里的开关是配合定义你们自己看吧其他的也是
+end)
 
-    end)
+Wan:Slider('滑块', 'Wan', 0, 0, 9999,false, function(b)
     
-    XKNB:Dropdown("Dropdown", "", { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, function(s)
-	print(s)
-    end)
+end)
 
-    XKNB:Textbox("输入文本", "", "输入", function(s)
-	
-    end)
+Wan:Textbox("输入", "Wan", "输入", function(c)
+  
+end)
+
+Wan:Dropdown("下拉式", "Wan", {
+    "额"
+}, function(d)
+    
+end)
