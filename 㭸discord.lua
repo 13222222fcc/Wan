4ccd285806c71ff805f6f0e661f4f1f22c564c2a
di🥺di🤓di <xjv_2025@qq.com>
2025-12-09 13:39:57 +0800

    Add HTML structure with styled text and imagedy

--- 㭸discord.lua
+++ 㭸discord.lua
@@ -1,29 +1,56 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/13222222fcc/Wan/refs/heads/main/UI.lua"))(); --这里是放你UI的地方也就是你的界面
+    local library = loadstring(game:HttpGet(('https://github.com/DevSloPo/Auto/raw/main/Ware-obfuscated.lua')))()
 
-local window = library:new("脚本") --这里是你的脚本名
-
-local Wans = window:Tab("通用",'图片ID') --这是选项卡必须加不然脚本用不了
-
-local Wan =Wans:section("通用",true) --这个也是
-
-Wan:Button("按钮", function()
-    --当然了这里是放脚本的地方比如放皮脚本之类的东西
-end)
-
-Wan:Toggle("开关", "Wan", false, function(a)
-    --这里的开关是配合定义你们自己看吧其他的也是
-end)
-
-Wan:Slider('滑块', 'Wan', 0, 0, 9999,false, function(b)
+    local window = library:new("脚本名字")
+    local XKHub = window:Tab("关于", "7733774602")
+    local XK = XKHub:section("示例", true)
     
-end)
-
-Wan:Textbox("输入", "Wan", "输入", function(c)
-  
-end)
-
-Wan:Dropdown("下拉式", "Wan", {
-    "额"
-}, function(d)
+    XK:Label("文本标签")
     
-end)
+    XK:Button(
+        "点击",
+        function()
+            
+        end)
+    
+    XK:Toggle("开关", "", false, function(state)
+
+    end)
+    
+    XK:Slider("滑块", "", 16, 0, 100, false, function(v)
+
+    end)
+    
+    XK:Dropdown("Dropdown", "", { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, function(s)
+	print(s)
+    end)
+
+    XK:Textbox("输入文本", "", "输入", function(s)
+	
+    end)    
+    
+    local XKHub = window:Tab("其他", "78892482588180")
+    local XKNB = XKHub:section("内容", true)
+    
+    XKNB:Label("文本标签")
+    
+    XKNB:Button(
+        "点击",
+        function()
+            
+        end)
+    
+    XKNB:Toggle("开关", "", false, function(state)
+
+    end)
+    
+    XKNB:Slider("滑块", "", 16, 0, 100, false, function(v)
+
+    end)
+    
+    XKNB:Dropdown("Dropdown", "", { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }, function(s)
+	print(s)
+    end)
+
+    XKNB:Textbox("输入文本", "", "输入", function(s)
+	
+    end)
